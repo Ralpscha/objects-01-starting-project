321126c33286a619919e94290bb3c8e934a947d1
Ralph van Schaik <ralph.van.schaik@me.com>
2020-12-03 10:52:09 +0100

    tm 217 special names

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,5 +1,9 @@
-const person = {
-    name: 'Ralph',
+const movieList = document.getElementById('movie-list');
+movieList.style.backgroundColor = 'red';
+movieList.style.display = 'block';
+
+let person = {
+    'first name': 'Ralph',
     age: 52,
     hobbies: ['Sports', 'Eating'],
     greet: function(){
@@ -7,4 +11,10 @@ const person = {
     }
 };
 
-person.greet();
+// person.greet();
+person.age = 31;
+delete person.age;
+person.isAdmin = true;
+console.log(person);
+console.log(person['first name']);
+
