f72d1503d71731c307b027f1df10ca601e8c011b
Ralph van Schaik <ralph.van.schaik@me.com>
2020-12-03 11:16:22 +0100

    tm 219 dynamic properties

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -2,10 +2,13 @@ const movieList = document.getElementById('movie-list');
 movieList.style.backgroundColor = 'red';
 movieList.style.display = 'block';
 
+const userChosenKeyName = 'level'; //via input verkregen, dit is een dummy
+
 let person = {
     'first name': 'Ralph',
     age: 52,
     hobbies: ['Sports', 'Eating'],
+    [userChosenKeyName]: '...',
     greet: function(){
         alert('Hi, there');
     },
@@ -16,6 +19,10 @@ let person = {
 person.age = 31;
 delete person.age;
 person.isAdmin = true;
+
+const keyName = 'last name';
+
 console.log(person);
 console.log(person['first name']);
+console.log(person[keyName]);
 console.log(person[1.5]);
